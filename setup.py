4e6a2c0e6bbc08c3c1bcc2604eb98ca16586ae1b
Hugo Defrance <defrance.hugo@gmail.com>
2014-09-25 14:44:27 +0200

    Addes setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='json_response',
+    version='0.1.0',
+    author=u'Hugo Defrance',
+    author_email='defrance.hugo@gmail.com',
+    packages=['json_response'],
+    url='https://github.com/koleror/django_json_response',
+    description='An implementation of django JsonResponse with multiple status codes',
+    long_description='An implementation of django JsonResponse with multiple status codes',
+    zip_safe=False,
+)
